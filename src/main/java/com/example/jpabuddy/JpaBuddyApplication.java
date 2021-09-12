f75e5e7a7c5b7e54932adfc5c952811672b0da82
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-09-12 13:22:57 +0530

    Initial commit

---
+++ src/main/java/com/example/jpabuddy/JpaBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpabuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBuddyApplication.class, args);
+    }
+
+}
